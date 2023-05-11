75181b70ee6cd27272c8caa312ac5427ece1d381
Vladislav Keller <skiffvlad90@gmail.com>
2023-05-11 22:05:56 +0300

    t6

--- src/js/task-04.js
+++ src/js/task-04.js
@@ -1,40 +1,3 @@
-const colors = [
-  '#FFFFFF',
-  '#2196F3',
-  '#4CAF50',
-  '#FF9800',
-  '#009688',
-  '#795548',
-  '#000000',
-  '#8B008B',
-  '#FF00FF',
-  '#FFD700',
-  '#FFA500',
-  '#00FF00',
-  '#00FFFF',
-  '#FF0000',
-  '#0000FF',
-  '#FFC0CB',
-  '#FFA07A',
-  '#FFB6C1',
-  '#FFDAB9',
-  '#FFDEAD',
-  '#FFE4B5',
-  '#FFE4C4',
-  '#FFE4E1',
-  '#FFEBCD',
-  '#FFEFD5',
-  '#FFF0F5',
-  '#FFF5EE',
-  '#FFF8DC',
-  '#FFFACD',
-  '#FFFAF0',
-  '#FFFAFA',
-  '#FFFF00',
-  '#FFFFE0',
-  '#FFFFF0',
-  '#FFFFFF',
-];
 const buttonDecrement = document.querySelector('[data-action="decrement"]');
 const buttonIncrement = document.querySelector('[data-action="increment"]');
 let counterValue = document.querySelector('#value');
@@ -96,21 +59,21 @@ buttonIncrement.addEventListener(
 
 // add random font size for counter text when click on buttonIncrement and buttonDecrement
 
-// buttonDecrement.addEventListener(
-//   'click',
-//   () => {
-//     document.querySelector('#value').style.fontSize = `${Math.floor(
-//       Math.random() * 100
-//     )}px`;
-//   },
-//   false
-// );
+buttonDecrement.addEventListener(
+  'click',
+  () => {
+    document.querySelector('#value').style.fontSize = `${Math.floor(
+      Math.random() * 100
+    )}px`;
+  },
+  false
+);
 
-// buttonIncrement.addEventListener('click', () => {
-//   document.querySelector('#value').style.fontSize = `${Math.floor(
-//     Math.random() * 100
-//   )}px`;
-// });
+buttonIncrement.addEventListener('click', () => {
+  document.querySelector('#value').style.fontSize = `${Math.floor(
+    Math.random() * 100
+  )}px`;
+});
 
 // add rabdom border size color radius for counter text when click on buttonIncrement and buttonDecrement
 
